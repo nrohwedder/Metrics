5c7fdcac8ca6b85b85065644eccf0839b9ecc205
Nils Rohwedder <nrohwedder@gmx.de>
2023-06-27 11:38:23 +0200

    Adding App

---
+++ App/DummyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DummyApp
+//
+//  Created by Nils Rohwedder on 27.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
